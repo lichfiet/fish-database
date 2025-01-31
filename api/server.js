ab62de817d5b7a64fb7b549e6b91498d00133ed1
Trevor Lichfield <sortaminty@gmail.com>
2025-01-30 20:44:42 -0700

    add todo

--- api/server.js
+++ api/server.js
@@ -1,1 +1,12 @@
-console.log("MEOW")
+
+// to do:
+//   - add health check
+//   - set up database communication
+//   - add a way to get all fish
+//   - add a way to delete a fish
+//   - add a way to edit a fish
+//   - add a way to add a fish
+//   - add a way to get a fish
+//   - containerize
+//   - add tests
+//   - add validation middleware
